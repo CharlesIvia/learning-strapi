3f0296ac403a79b414bc47754e64fc8aa37af859
Charles Ivia <iviacharles@gmail.com>
2022-10-07 11:21:40 +0300

    apollo client and gql

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,34 @@
-import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 
+//Page and layout imports
 import { Home } from "./pages/Home";
 import { Categories } from "./pages/Categories";
 import { Review } from "./pages/Review";
 import { SiteHeader } from "./components/SiteHeader";
 
+//apollo client
+
+const client = new ApolloClient({
+  uri: "http://localhost:1337/graphql",
+  cache: new InMemoryCache(),
+});
+
 function App() {
   return (
-    <div className="App">
-      <Router>
-        <SiteHeader />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/categories/:id" element={<Categories />} />
-          <Route path="/review/:id" element={<Review />} />
-          <Route path="*" element={<Home />} />
-        </Routes>
-      </Router>
-    </div>
+    <Router>
+      <ApolloProvider client={client}>
+        <div className="App">
+          <SiteHeader />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/categories/:id" element={<Categories />} />
+            <Route path="/review/:id" element={<Review />} />
+            <Route path="*" element={<Home />} />
+          </Routes>
+        </div>
+      </ApolloProvider>
+    </Router>
   );
 }
 
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,3 +1,34 @@
+//Required imports
+import { Link } from "react-router-dom";
+import { useFetch } from "../hooks/useFetch";
+import { useQuery, gql } from "@apollo/client";
+
 export const Home = () => {
-  return <div>Home</div>;
+  const { loading, error, data } = useFetch(
+    "http://localhost:1337/api/reviews"
+  );
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+
+  if (data) {
+    console.log(data.data);
+  }
+
+  return (
+    <div>
+      {data &&
+        data.data.map((review) => (
+          <div key={review.id} className="review-card">
+            <div className="rating">{review.attributes.Rating}</div>
+            <h2>{review.attributes.Name}</h2>
+
+            <small>console list</small>
+
+            <p>{review.attributes.Body.substring(0, 200)}...</p>
+            <Link to={`/details/${review.id}`}>Read more</Link>
+          </div>
+        ))}
+    </div>
+  );
 };
