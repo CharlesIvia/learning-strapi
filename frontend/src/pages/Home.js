636a5ab2b5533a55cbac61f454e11fc360ed267a
Charles Ivia <iviacharles@gmail.com>
2022-10-07 11:49:51 +0300

    fetch all reviews using gql

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,24 +1,37 @@
 //Required imports
 import { Link } from "react-router-dom";
-import { useFetch } from "../hooks/useFetch";
+//import { useFetch } from "../hooks/useFetch";
 import { useQuery, gql } from "@apollo/client";
 
+const REVIEWS = gql`
+  query GetReviews {
+    reviews {
+      data {
+        id
+        attributes {
+          Name
+          Rating
+          Body
+        }
+      }
+    }
+  }
+`;
+
 export const Home = () => {
-  const { loading, error, data } = useFetch(
-    "http://localhost:1337/api/reviews"
-  );
+  const { loading, data, error } = useQuery(REVIEWS);
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error :(</p>;
 
   if (data) {
-    console.log(data.data);
+    console.log(data.reviews.data);
   }
 
   return (
     <div>
       {data &&
-        data.data.map((review) => (
+        data.reviews.data.map((review) => (
           <div key={review.id} className="review-card">
             <div className="rating">{review.attributes.Rating}</div>
             <h2>{review.attributes.Name}</h2>
@@ -26,7 +39,7 @@ export const Home = () => {
             <small>console list</small>
 
             <p>{review.attributes.Body.substring(0, 200)}...</p>
-            <Link to={`/details/${review.id}`}>Read more</Link>
+            <Link to={`/details/${review.attributes.id}`}>Read more</Link>
           </div>
         ))}
     </div>
